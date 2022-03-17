60a1fd80cab593ec860fb9f33110aabec1feb684
lucascarrafa <lucascarrafa@hotmail.com>
2022-03-17 19:58:34 -0300

    aula-17032022

---
+++ exercicios/componentes/Primeiro.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Text } from 'react-native'
+import Fonte from './estilo'
+
+export default () => <Text style={Fonte.fontGrande}>Primeiro!!</Text>
---
+++ exercicios/componentes/estilo.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native"
+
+export default StyleSheet.create({
+    fontGrande:{
+        fontSize: 30
+    },
+    fontPequena:{
+        fontSize: 15,
+    }
+})
