052f33ee0e7846db93a9d5b1e7d80f86db952725
lucascarrafa <lucascarrafa@hotmail.com>
2022-03-21 20:01:24 -0300

    aula-21032021

--- exercicios/App.js
+++ exercicios/App.js
@@ -3,13 +3,14 @@ import { View, Text, StyleSheet } from 'react-native'
 
 import Primeiro from './componentes/Primeiro'
 import Monitor from './componentes/Monitor'
-
+import ValorMaxMin from './componentes/ValorMaxMin'
 
 export default () => (
     <View style={tela.App}>
         <Primeiro />
         <Primeiro />
         <Monitor />
+        <ValorMaxMin min={10} max={23} nome="lucas"/>
     </View>
 )
 
