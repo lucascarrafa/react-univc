2e4e07cbbeeec9941c47ffd03c85f77f152f6f98
lucascarrafa <lucascarrafa@hotmail.com>
2022-04-28 20:59:56 -0300

    lista

---
+++ exercicios/componentes/produtos/listaprodutos.js
@@ -0,0 +1,13 @@
+export default[
+    {id:1,nome:'Picanha', preco:45.90},
+    {id:2,nome:'Nan',preco:56.90},
+    {id:3,nome:'Hall',preco:4.90},
+    {id:4,nome:'Sabao em barra Teiu',preco:5.90},
+    {id:5,nome:'Vinho Erva Doce',preco:100.00},
+    {id:6,nome:'Garoto Serenata',preco:0.90},
+    {id:7,nome:'Sabonete Palmolive',preco:1.90},
+    {id:8,nome:'Absolut',preco:134.80},
+    {id:9,nome:'Pao de forma',preco:8.99},
+    {id:10,nome:'Mentos',preco:10.99},
+    {id:11,nome:'Shampoo',preco:7.50},
+]
