7e212b99ff80678a053c3af80c88d55e92568dc4
lucascarrafa <lucascarrafa@hotmail.com>
2022-05-07 22:47:52 -0300

    conexao com api

--- exercicios/componentes/EntradaDados.js
+++ exercicios/componentes/EntradaDados.js
@@ -3,13 +3,18 @@ import { View, TextInput, Text } from 'react-native'
 import Estilo from './estilo'
 import VerificaImparPar from './ParImpar'
 import produtos from './produtos/listaprodutos'
+import axios from "axios"
+
 
 export default () => {
     const [valor, setValor] = useState('')
+    const [lista, setLista] = useState([])
+
     function obterListaDesconto(){
-        return produtos.map(p =>{
+        getAdvice()
+        return lista.map(p =>{
             return(
-                <Text key={p.id}>
+                <Text style={Estilo.fontPequena} key={p.id}>
                     {p.id}) {p.nome} custa R$ {(p.preco-p.preco*(valor/100)).toFixed(2)} com {valor}% desconto
                 </Text>
             )
@@ -17,13 +22,24 @@ export default () => {
         )
     }
 
+    const getAdvice = () => {
+        axios
+            .get("http://192.168.10.102:5000/lista")
+            .then((response) => {
+                setLista(response.data.map(aux=>({id: aux.id,nome: aux.nome, preco: aux.preco})))
+            });
+    };
+
     return(
         <View>
+            <Text style={Estilo.fontPequena}>Desconto:</Text>
             <TextInput 
+            style={{ height: 25, borderColor: '#80dead', borderWidth: 2 }}
             placeholder='Digite o desconto' 
             value={valor} 
             onChangeText={valor => setValor(valor)}>
             </TextInput>
+            <Text style={Estilo.fontGrande}>Lista</Text>
             {obterListaDesconto()}
         </View>
     )
